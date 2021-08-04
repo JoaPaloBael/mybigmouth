257fa96c43d5ef5b619696c7b908f64e46947ff1
Johanna L. Palo Bael <joa.palobael@gmail.com>
2021-08-03 21:52:34 -0300

    Refactor de funciones en tienda.js

--- js/tienda.js
+++ js/tienda.js
@@ -1,127 +1,127 @@
-//Carrito de compras
+//-------------------------------------------------------CARRITO DE COMPRAS
 
-//1. Obtener referencia del DOM
-//botón de añadir al carrito
-const sumarAlCarritoBtn = document.querySelectorAll('.sumarAlCarro');
-//Botón de comprar
-const comprarBtn = document.querySelector('.comprarButton');
+//1. Obtener referencias del DOM
 
-//2. Capturar interacción del usuario
-//forEach para colocar evento onclick
-sumarAlCarritoBtn.forEach((obj) => {
-  obj.addEventListener('click', btnCarritoClickeado);
+const sumarBtn = document.querySelectorAll('.sumarBtn'); //btn de añadir al carrito
+const comprarBtn = document.querySelector('.comprarBtn'); //Btn de comprar
+const contenedorDiscosAgregados = document.querySelector('.contenedorDiscosAgregados');
+
+//2. Capturear interacción del usuario - forEach para colocar evento onclick
+
+sumarBtn.forEach((obj) => {
+  obj.addEventListener('click', sumarBtnClickeado);
 });
 
-// 3. Reaccionar a la interacción del usuario
 comprarBtn.addEventListener('click', comprarBtnClickeado);
 
-const shoppingCartItemsContainer = document.querySelector(
-  '.shoppingCartItemsContainer'
-);
+// 3. Reaccionar a la interacción del usuario
+// Una vez clickeado el boton de añadir, se captura la información en variables.
 
-function btnCarritoClickeado(event) {
-  const button = event.target;
-  const item = button.closest('.item');
+function sumarBtnClickeado(e) {
+  const button = e.target;
+  const disco = button.closest('.disco');
 
-  const itemPrice = item.querySelector('.item-price').textContent;
-  const itemTitle = item.querySelector('.item-title').textContent;
-  const itemImage = item.querySelector('.item-image').src;
+  const precioDisco = disco.querySelector('.precioDisco').textContent;
+  const tituloDisco = disco.querySelector('.tituloDisco').textContent;
+  const imagenDisco = disco.querySelector('.imagenDisco').src;
 
-  addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+  sumarAlCarrito(tituloDisco, precioDisco, imagenDisco);
 }
 
-function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
-  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
-    'shoppingCartItemTitle'
-  );
-  for (let i = 0; i < elementsTitle.length; i++) {
-    if (elementsTitle[i].innerText === itemTitle) {
-      let elementQuantity = elementsTitle[
+//4. Función para agregar los items al contenedorDiscosAgregados con los parametros de 3.
+function sumarAlCarrito(tituloDisco, precioDisco, imagenDisco) {
+  const tuCompraSeccion = contenedorDiscosAgregados.getElementsByClassName('nombreDiscoEnCarrito');
+  //Bucle for
+  for (let i = 0; i < tuCompraSeccion.length; i++) {
+    if (tuCompraSeccion[i].innerText === tituloDisco) {
+      let cantidadDiscos = tuCompraSeccion[
         i
       ].parentElement.parentElement.parentElement.querySelector(
-        '.shoppingCartItemQuantity'
+        '.cantidadDeCadaDiscoEnCarritoNum'
       );
-      elementQuantity.value++;
+      cantidadDiscos.value++;
+        //bootstrap - Cartel de incremento de producto
       $('.toast').toast('show');
-      updateShoppingCartTotal();
+      actualizarTotalCarrito();
       return;
     }
   }
 
-  const shoppingCartRow = document.createElement('div');
-  const shoppingCartContent = `
-  <div class="row shoppingCartItem">
-        <div class="col-6">
+  //5. Modificar el DOM en la sección de "tu compra"
+  const filaTuCompra = document.createElement('div');
+  const contenidoCarrito = `
+  <div class="row itemCarrito">
+        <div class="col-8">
             <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+                <img src=${imagenDisco} class="shopping-cart-image">
+                <h6 class="shopping-cart-tituloDisco nombreDiscoEnCarrito text-truncate ml-3 mb-0">${tituloDisco}</h6>
             </div>
         </div>
         <div class="col-2">
             <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+                <p class="precioDisco mb-0 precioDiscoEnCarritoNum">${precioDisco}</p>
             </div>
         </div>
-        <div class="col-4">
+        <div class="col-2">
             <div
                 class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
+                <input class="shopping-cart-quantity-input cantidadDeCadaDiscoEnCarritoNum" type="number"
+                    value="1" min="1" max="20">
                 <button class="btn btn-danger buttonDelete" type="button">X</button>
             </div>
         </div>
-    </div>`;
-  shoppingCartRow.innerHTML = shoppingCartContent;
-  shoppingCartItemsContainer.append(shoppingCartRow);
+    </div>
+    `;
+  filaTuCompra.innerHTML = contenidoCarrito;
+  contenedorDiscosAgregados.append(filaTuCompra);
 
-  shoppingCartRow
+  //6. Eliminar Disco del carrito
+  filaTuCompra
     .querySelector('.buttonDelete')
-    .addEventListener('click', removeShoppingCartItem);
+    .addEventListener('click', eliminarDiscoCarrito);
 
-  shoppingCartRow
-    .querySelector('.shoppingCartItemQuantity')
-    .addEventListener('change', quantityChanged);
+  //7. Actualizar la cantidad de cada disco
+  filaTuCompra
+    .querySelector('.cantidadDeCadaDiscoEnCarritoNum')
+    .addEventListener('change', cambiarCantidad);
 
-  updateShoppingCartTotal();
+  actualizarTotalCarrito();
+
 }
 
-function updateShoppingCartTotal() {
+//8. Actualizar precio total del carrito
+function actualizarTotalCarrito() {
+  //---Variables locales
   let total = 0;
-  const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+  const precioFinalCarrito = document.querySelector('.precioFinalCarrito');
+  const itemsCarrito = document.querySelectorAll('.itemCarrito');
 
-  const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
-
-  shoppingCartItems.forEach((shoppingCartItem) => {
-    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemPrice'
-    );
-    const shoppingCartItemPrice = Number(
-      shoppingCartItemPriceElement.textContent.replace('$', '')
-    );
-    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
-      '.shoppingCartItemQuantity'
-    );
-    const shoppingCartItemQuantity = Number(
-      shoppingCartItemQuantityElement.value
-    );
-    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+  // forEach para recorrer el carrito
+  itemsCarrito.forEach((itemCarrito) => {
+    const precioDiscoEnCarrito = itemCarrito.querySelector('.precioDiscoEnCarritoNum');
+    const precioDiscoEnCarritoNum = Number(precioDiscoEnCarrito.textContent.replace('$', ''));
+    const cantidadDeCadaDiscoEnCarrito = itemCarrito.querySelector('.cantidadDeCadaDiscoEnCarritoNum');
+    const cantidadDeCadaDiscoEnCarritoNum = Number(cantidadDeCadaDiscoEnCarrito.value);
+    total = total + precioDiscoEnCarritoNum * cantidadDeCadaDiscoEnCarritoNum;
   });
-  shoppingCartTotal.innerHTML = `${total.toFixed(2)}$`;
+
+  precioFinalCarrito.innerHTML = `${total.toFixed(2)}$`;
 }
 
-function removeShoppingCartItem(event) {
+//9. Funciones de Actualizar cantidad, eliminar disco y actualizar precio total
+function eliminarDiscoCarrito(event) {
   const buttonClicked = event.target;
-  buttonClicked.closest('.shoppingCartItem').remove();
-  updateShoppingCartTotal();
+  buttonClicked.closest('.itemCarrito').remove();
+  actualizarTotalCarrito();
 }
 
-function quantityChanged(event) {
+function cambiarCantidad(event) {
   const input = event.target;
   input.value <= 0 ? (input.value = 1) : null;
-  updateShoppingCartTotal();
+  actualizarTotalCarrito();
 }
 
 function comprarBtnClickeado() {
-  shoppingCartItemsContainer.innerHTML = '';
-  updateShoppingCartTotal();
+  contenedorDiscosAgregados.innerHTML = '';
+  actualizarTotalCarrito();
 }
